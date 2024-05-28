011d1a2cfbecb87b08962e85894b60f4ef83ba56
Cristian Tomulescu <150183775+crislynch@users.noreply.github.com>
2024-05-28 23:25:59 +0200

    creazione programma

---
+++ src/main/java/com/cris/mannagg/Controller.java
@@ -0,0 +1,12 @@
+package com.cris.mannagg;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/")
+    public String getProperty() {
+        return "devName: YourName";
+    }
+}
